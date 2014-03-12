8240fc6bddf71cc4d718fbf086e2305fc5d66d51
AurelineGrange <aurelinegrange@me.com>
2014-03-12 21:48:46 +0100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpaceHearts::Application.config.secret_key_base = 'edbf4791e32606be15a5d8d770e52bcd2762deaa6a5846eea813c0a7795266bbd800bc51f67177ce3b4c37f29699c1265537d7bf04af677640ff908a51ea5f68'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SpaceHearts::Application.config.session_store :cookie_store, key: '_Space_Hearts_session'
