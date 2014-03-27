3b6719c864d00a18e63e1deff6f718e0d50a2f54
AurelineGrange <aurelinegrange@me.com>
2014-03-28 00:13:56 +0100

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 SpaceHearts::Application.routes.draw do
+
+  resources :users
   root 'static_pages#home'
 
-  get "users/new"
   match '/signup', to: 'users#new', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Aureline Grange"
+    email    "aureline@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
