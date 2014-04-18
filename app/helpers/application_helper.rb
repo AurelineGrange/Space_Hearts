9742da9ee56471dda831b1bb82032a2b1a48df35
jcgasche <jc.gasche@mac.com>
2014-04-18 23:11:04 +0200

    ajax for iphone secret key check

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,9 +10,9 @@ def full_title(page_title)
 end
 
 def random_key
-	adjectives = ['Fresh', 'Happy', 'Devoted' , 'Loving' , 'Sympathetic' , 'Believing' , 'Supportive' , 'Enlightened' , 'Clever' , 'Involved' ,
-			'Intent', 'Joyful',  'Liking', 'Sweet', 'Pride', 'Faith' ,'Thrilled', 'Serene', 'Cute']
-	nouns = ['Crocodile', 'Angel', 'Sweetheart', 'Cupcake' , 'Sparkle' , 'Honey' , 'Sugar' , 'Love' , 'Sweetie', 'Cutie','Baby']
+	adjectives = ['Fantastic', 'Happy', 'Devoted' , 'Loving' , 'Sympathetic' , 'Believing' , 'Supportive' , 'Enlightened' , 'Clever' , 'Involved' ,
+			'Intent', 'Joyful',  'Liking', 'Sweet', 'Darling', 'Kind' ,'Thrilled', 'Serene', 'Cute']
+	nouns = ['Crocodile', 'Angel', 'Sweetheart', 'Cupcake' , 'Sparkle' , 'Honey' , 'Sugar' , 'Love' , 'Sweetie', 'Cutie','Dolphin']
 	key= adjectives[Random.rand(19)].to_s<<"-"<<nouns[Random.rand(11)].to_s<<"-"<<(1_000 + Random.rand(9_999 - 1_000)).to_s 
 	while(Micropost.find_by(secret_key: key))
 	key= adjectives[Random.rand(19)].to_s<<"-"<<nouns[Random.rand(11)].to_s<<"-"<<(1_000 + Random.rand(9_999 - 1_000)).to_s 
